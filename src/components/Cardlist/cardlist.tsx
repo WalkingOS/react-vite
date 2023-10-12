9009113e180206d3f11df94fc7ad35f676aa1fbb
Emil Lehmann <emil-thomas.lehmann@telekom.de>
2023-10-12 09:56:01 +0200

    feat: Move fetching to custom hook

--- src/components/Cardlist/cardlist.tsx
+++ src/components/Cardlist/cardlist.tsx
@@ -1,14 +1,14 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Cards from "../Cards/cards";
 import { ArticlesData } from "../../main";
 import "./cardlist.css";
 import "./search.css";
 import Search from "../Search/search.tsx";
+import useFetchArticles from "../../utils/useFetch.tsx";
 
 const ArticlesModule = (): JSX.Element => {
     const [keyword, setKeyword] = useState("");
-    const [articles, setArticles] = useState<ArticlesData[] | null>(null);
-    const [loading, setLoading] = useState(true);
+    const {articles, isLoading} = useFetchArticles("https://api.jsonbin.io/v3/qs/6526ac7012a5d376598a57fe");
 
     const search = (articles: ArticlesData[], keyword: string): ArticlesData[] => {
         if (!keyword && articles) {
@@ -18,22 +18,6 @@ const ArticlesModule = (): JSX.Element => {
         return articles.filter(article => (
             article.title.toLowerCase().includes(keyword.toLowerCase())))
     }
-
-    useEffect(() => {
-        const fetchArticles = async (url: string) => {
-            try {
-                const response = await fetch(url);
-                const result = await response.json();
-                const articlePromise = result.record;
-                setArticles(articlePromise.record);
-                setLoading(false);
-            } catch (error) {
-                console.error("Error fetching articles: ", error);
-            }
-        }
-
-        fetchArticles("https://api.jsonbin.io/v3/qs/6526ac7012a5d376598a57fe");
-    }, [])
 
     return (
         <>
@@ -54,7 +38,7 @@ const ArticlesModule = (): JSX.Element => {
             <div className="container">
                 <div id="articles">
                         {
-                            (!loading && articles) ? (
+                            (!isLoading && articles) ? (
                                 <Cards articles={search(articles, keyword)}/>
                             ) : <div className="articles-loading">Loading...</div>
                         }
